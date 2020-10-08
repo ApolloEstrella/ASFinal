069ec697339354ff36c6f934cde017a88f6d18bc
ApolloEstrella <34788252+ApolloEstrella@users.noreply.github.com>
2020-10-08 23:00:12 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { makeStyles, createStyles, useTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Menu, MenuItem, useMediaQuery } from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import Login  from "../src/components/authentications/login";
+import Register from "../src/components/authentications/register";
+import { Route, Switch } from "react-router-dom";
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+ 
+  const App = props => {
+    const { history } = props;
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+
+    const handleMenu = event => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClick = pageURL => {
+      history.push(pageURL);
+      setAnchorEl(null);
+    };
+
+    const handleButtonClick = pageURL => {
+      history.push(pageURL);
+    };
+
+    const menuItems = [
+      {
+        key:0,
+        menuTitle: "Login",
+        pageURL: "/login",
+      },
+      {
+        key:1,
+        menuTitle: "Register",
+        pageURL: "/register",
+      } 
+    ];
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <div className={classes.root}>
+              <AppBar position="static">
+                <Toolbar>
+                  <Typography variant="h6" className={classes.title}>
+                    THE DAILY PLANET
+                  </Typography>
+                  {isMobile ? (
+                    <>
+                      <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={handleMenu}
+                      >
+                        <MenuIcon />
+                      </IconButton>
+                      <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                          vertical: "top",
+                          horizontal: "right",
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "right",
+                        }}
+                        open={open}
+                        onClose={() => setAnchorEl(null)}
+                      >
+                        {menuItems.map((menuItem, index) => {
+                          const { menuTitle, pageURL} = menuItem;
+                          return (
+                            <MenuItem key={index} onClick={() => handleMenuClick(pageURL)}>
+                             {menuTitle}
+                            </MenuItem>
+                          );
+                        })}
+                      </Menu>
+                    </>
+                  ) : (
+                    <>
+                      <Button
+                        color="inherit"
+                        onClick={() => handleButtonClick("/login")}
+                      >
+                        Login
+                      </Button>
+                      <Button
+                        color="inherit"
+                        onClick={() => handleButtonClick("/register")}
+                      >
+                        Register
+                      </Button>
+                    </>
+                  )}
+                </Toolbar>
+              </AppBar>
+            </div>
+          </Grid>
+        </Grid>
+        <Switch>
+          <Route exact from="/login" render={(props) => <Login {...props} />} />           
+          <Route exact path="/register" render={(props) => <Register {...props} />} />
+        </Switch>
+      </div>
+    );
+  }
+
+export default withRouter(App);
---
+++ src/components/authentications/register.tsx
@@ -0,0 +1,313 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  TextField,
+  Button,
+  makeStyles,
+  createStyles,
+  Theme,
+} from "@material-ui/core";
+import { Formik, Form, FormikProps } from "formik";
+import * as Yup from "yup";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: "450px",
+      display: "block",
+      margin: "0 auto",
+    },
+    textField: {
+      "& > *": {
+        width: "100%",
+      },
+    },
+    submitButton: {
+      marginTop: "24px",
+    },
+    button: {
+      marginRight: "20px",
+    },
+    title: { textAlign: "center" },
+    successMessage: { color: "green" },
+    errorMessage: { color: "red" },
+  })
+);
+
+interface ISignUpForm {
+  user: {
+    fullName: string;
+    password: string;
+    confirmPassword: string;
+    email: string;
+  };
+}
+
+interface IFormStatus {
+  message: string;
+  type: string;
+}
+
+interface IFormStatusProps {
+  [key: string]: IFormStatus;
+}
+
+const formStatusProps: IFormStatusProps = {
+  success: {
+    message: "Registered successfully.",
+    type: "success",
+  },
+  duplicate: {
+    message: "Email-id already exist. Please use different email-id.",
+    type: "error",
+  },
+  error: {
+    message: "Something went wrong. Please try again.",
+    type: "error",
+  },
+};
+
+const SignUp: React.FunctionComponent = () => {
+  const classes = useStyles();
+  const [displayFormStatus, setDisplayFormStatus] = useState(false);
+  const [formStatus, setFormStatus] = useState<IFormStatus>({
+    message: "",
+    type: "",
+  });
+  const history = useHistory();
+
+  const handleButtonClick = (pageURL: string) => {
+    history.push(pageURL);
+  };
+
+  const createNewUser = async (data: ISignUpForm, resetForm: Function) => {
+    try {
+      // API call integration will be here. Handle success / error response accordingly.
+      if (data) {
+        setFormStatus(formStatusProps.success);
+        resetForm({});
+      }
+    } catch (error) {
+      const response = error.response;
+      if (response.data === "user already exist" && response.status === 400) {
+        setFormStatus(formStatusProps.duplicate);
+      } else {
+        setFormStatus(formStatusProps.error);
+      }
+    } finally {
+      setDisplayFormStatus(true);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Formik
+        initialValues={{
+          user: {
+            fullName: "",
+            password: "",
+            confirmPassword: "",
+            email: "",
+          },
+        }}
+        onSubmit={(values: ISignUpForm, actions) => {
+          createNewUser(values, actions.resetForm);
+          setTimeout(() => {
+            actions.setSubmitting(false);
+          }, 500);
+        }}
+        validationSchema={Yup.object().shape({
+          user: Yup.object().shape({
+            email: Yup.string().email().required("Email is required!"),
+            fullName: Yup.string().required("Please enter full name"),
+            password: Yup.string()
+              .matches(
+                /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,20}\S$/
+              )
+              .required(
+                "Please valid password. One uppercase, one lowercase, one special character and no spaces"
+              ),
+            confirmPassword: Yup.string()
+              .required("Required")
+              .test("password-match", "Password must match", function (value) {
+                return this.parent.password === value;
+              }),
+          }),
+        })}
+      >
+        {(props: FormikProps<ISignUpForm>) => {
+          const {
+            values,
+            touched,
+            errors,
+            handleBlur,
+            handleChange,
+            isSubmitting,
+          } = props;
+          return (
+            <Form>
+              <h1 className={classes.title}>Register</h1>
+              <Grid container justify="space-around" direction="row">
+                <Grid
+                  item
+                  lg={10}
+                  md={10}
+                  sm={10}
+                  xs={10}
+                  className={classes.textField}
+                >
+                  <TextField
+                    name="user.fullName"
+                    id="user.fullName"
+                    label="Full Name"
+                    value={values.user.fullName}
+                    type="text"
+                    helperText={
+                      errors.user?.fullName && touched.user?.fullName
+                        ? errors.user?.fullName
+                        : "Enter your full name."
+                    }
+                    error={
+                      errors.user?.fullName && touched.user?.fullName
+                        ? true
+                        : false
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  lg={10}
+                  md={10}
+                  sm={10}
+                  xs={10}
+                  className={classes.textField}
+                >
+                  <TextField
+                    name="user.password"
+                    id="user.password"
+                    label="Password"
+                    value={values.user.password}
+                    type="password"
+                    helperText={
+                      errors.user?.password && touched.user?.password
+                        ? "Please valid password. One uppercase, one lowercase, one special character and no spaces"
+                        : "One uppercase, one lowercase, one special character and no spaces"
+                    }
+                    error={
+                      errors.user?.password && touched.user?.password
+                        ? true
+                        : false
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  lg={10}
+                  md={10}
+                  sm={10}
+                  xs={10}
+                  className={classes.textField}
+                >
+                  <TextField
+                    name="user.confirmPassword"
+                    id="user.confirmPassword"
+                    label="Confirm password"
+                    value={values.user.confirmPassword}
+                    type="password"
+                    helperText={
+                      errors.user?.confirmPassword &&
+                      touched.user?.confirmPassword
+                        ? errors.user?.confirmPassword
+                        : "Re-enter password to confirm"
+                    }
+                    error={
+                      errors.user?.confirmPassword &&
+                      touched.user?.confirmPassword
+                        ? true
+                        : false
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  lg={10}
+                  md={10}
+                  sm={10}
+                  xs={10}
+                  className={classes.textField}
+                >
+                  <TextField
+                    name="user.email"
+                    id="user.email"
+                    label="Email-id"
+                    value={values.user.email}
+                    type="email"
+                    helperText={
+                      errors.user?.email && touched.user?.email
+                        ? errors.user?.email
+                        : "Enter email-id"
+                    }
+                    error={
+                      errors.user?.email && touched.user?.email ? true : false
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  lg={10}
+                  md={10}
+                  sm={10}
+                  xs={10}
+                  className={classes.submitButton}
+                >
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="secondary"
+                    disabled={isSubmitting}
+                    onClick={() => handleButtonClick("/")}
+                  >
+                    Cancel
+                  </Button>
+                  {displayFormStatus && (
+                    <div className="formStatus">
+                      {formStatus.type === "error" ? (
+                        <p className={classes.errorMessage}>
+                          {formStatus.message}
+                        </p>
+                      ) : formStatus.type === "success" ? (
+                        <p className={classes.successMessage}>
+                          {formStatus.message}
+                        </p>
+                      ) : null}
+                    </div>
+                  )}
+                </Grid>
+              </Grid>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+
+export default SignUp;
